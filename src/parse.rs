1b1e82a571450ee4eca4c1784496e1a241e28861
Collin Van Dyck <collinvandyck@gmail.com>
2024-05-04 18:21:08 -0400

    wip

--- src/parse.rs
+++ src/parse.rs
@@ -11,7 +11,34 @@ impl Parser {
         Self { tokens, current: 0 }
     }
 
+    fn expression(&mut self) -> Expr {
+        self.equality()
+    }
+
+    // equality → comparison ( ( "!=" | "==" ) comparison )* ;
     fn equality(&mut self) -> Expr {
+        let mut expr = self.comparison();
+        while self.match_any([TokenType::BangEqual, TokenType::EqualEqual]) {
+            let op = self.previous();
+            let right = self.comparison();
+            expr = Expr::binary(expr, op, right);
+        }
+        expr
+    }
+
+    fn comparison(&mut self) -> Expr {
+        todo!()
+    }
+
+    fn current(&self) -> Token {
+        todo!()
+    }
+
+    fn previous(&self) -> Token {
+        todo!()
+    }
+
+    fn match_any(&mut self, typs: impl IntoIterator<Item = TokenType>) -> bool {
         todo!()
     }
 }
