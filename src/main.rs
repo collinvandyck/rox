678fec738d10cb5324f81e4255cb4fc8aa402740
Collin Van Dyck <collinvandyck@gmail.com>
2024-05-03 22:24:47 -0400

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
+#![allow(unused)]
+
+use std::path::PathBuf;
+
+use clap::Parser;
 use tracing::info;
 
+#[derive(Debug, clap::Parser)]
+struct Args {
+    script: PathBuf,
+}
+
 fn main() {
+    let args = Args::parse();
     tracing_subscriber::fmt().init();
     info!("Hello, world!");
 }
