71937e266e601f98bbbff37d50c3e31069c4d5fc
Collin Van Dyck <collinvandyck@gmail.com>
2024-05-05 15:45:00 -0400

    wip

--- src/stmt.rs
+++ src/stmt.rs
@@ -15,3 +15,10 @@ pub struct ExprStmt {
 pub struct PrintStmt {
     pub expr: Expr,
 }
+
+pub trait StmtVisitor {
+    type Output;
+
+    fn visit_print(&mut self, expr: &PrintStmt) -> Self::Output;
+    fn visit_expr(&mut self, expr: &ExprStmt) -> Self::Output;
+}
