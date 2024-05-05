a955a22a5340a659830b060697c157d5d09a840d
Collin Van Dyck <collinvandyck@gmail.com>
2024-05-05 12:28:12 -0400

    wip

--- src/interpret.rs
+++ src/interpret.rs
@@ -3,3 +3,20 @@ use crate::prelude::*;
 pub struct Interpreter {}
 
 pub enum Value {}
+
+impl ExprVisitor for Interpreter {
+    type Output = Value;
+
+    fn visit_binary(&mut self, expr: &BinaryExpr) -> Self::Output {
+        todo!()
+    }
+    fn visit_literal(&mut self, expr: &LiteralExpr) -> Self::Output {
+        todo!()
+    }
+    fn visit_unary(&mut self, expr: &UnaryExpr) -> Self::Output {
+        todo!()
+    }
+    fn visit_group(&mut self, expr: &GroupExpr) -> Self::Output {
+        todo!()
+    }
+}
