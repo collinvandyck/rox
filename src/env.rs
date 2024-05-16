97546a58bda3c8f313762e50ea6179a1efabd655
Collin Van Dyck <collinvandyck@gmail.com>
2024-05-16 09:25:43 -0400

    wip

--- src/env.rs
+++ src/env.rs
@@ -1,13 +1,31 @@
 use crate::prelude::*;
-use std::{borrow::Borrow, cell::RefCell, collections::HashMap, rc::Rc};
+use std::{
+    borrow::{Borrow, BorrowMut},
+    cell::RefCell,
+    collections::HashMap,
+    rc::Rc,
+};
 
-#[derive(thiserror::Error, Debug, strum_macros::EnumIs)]
+#[derive(thiserror::Error, Debug, strum_macros::EnumIs, derive_more::From)]
 pub enum EnvError {
     #[error("undefined variable in assign '{}'", name)]
     UndefinedAssign { name: String },
 
     #[error("undefined variable '{}'", token.lexeme)]
     NotFound { token: Token },
+}
+
+impl EnvError {
+    fn undefined_assign(name: impl AsRef<str>) -> Self {
+        Self::UndefinedAssign {
+            name: name.as_ref().to_string(),
+        }
+    }
+    fn not_found(token: &Token) -> Self {
+        Self::NotFound {
+            token: token.clone(),
+        }
+    }
 }
 
 #[derive(Clone, Default)]
@@ -17,23 +35,17 @@ pub struct Env {
 
 impl Env {
     pub fn assign(&self, name: impl AsRef<str>, val: impl Into<Literal>) -> Result<(), EnvError> {
-        self.inner.borrow_mut().assign(name, val)
+        self.inner.as_ref().borrow_mut().assign(name, val)
     }
     pub fn define(&self, name: impl AsRef<str>, val: impl Into<Literal>) {
-        let val = val.into();
-        self.inner
-            .borrow_mut()
-            .vars
-            .insert(name.as_ref().to_string(), val);
+        self.inner.as_ref().borrow_mut().define(name, val);
     }
-
     pub fn get(&self, token: &Token) -> Result<Literal, EnvError> {
         self.inner.as_ref().borrow().get(token)
     }
-
     pub fn child(&self) -> Self {
         let inner = EnvInner {
-            parent: Some(Rc::new(RefCell::new(self.clone()))),
+            parent: Some(Rc::new(self.clone())),
             ..Default::default()
         };
         Self {
@@ -44,20 +56,23 @@ impl Env {
 
 #[derive(Default)]
 pub struct EnvInner {
-    parent: Option<Rc<RefCell<Env>>>,
+    parent: Option<Rc<Env>>,
     vars: HashMap<String, Literal>,
 }
 
 impl EnvInner {
+    fn define(&mut self, name: impl AsRef<str>, val: impl Into<Literal>) {
+        self.vars.insert(name.as_ref().to_string(), val.into());
+    }
     fn assign(&mut self, name: impl AsRef<str>, val: impl Into<Literal>) -> Result<(), EnvError> {
         if let Some(v) = self.vars.get_mut(name.as_ref()) {
             *v = val.into();
-            Ok(())
-        } else {
-            Err(EnvError::UndefinedAssign {
-                name: name.as_ref().into(),
-            })
+            return Ok(());
         }
+        if let Some(parent) = &self.parent {
+            return parent.as_ref().assign(name, val);
+        }
+        Err(EnvError::undefined_assign(name))
     }
     fn get(&self, token: &Token) -> Result<Literal, EnvError> {
         let f: Option<&Literal> = self.vars.get(token.lexeme.as_ref());
@@ -65,11 +80,9 @@ impl EnvInner {
             return Ok(f.clone());
         }
         if let Some(parent) = &self.parent {
-            return parent.as_ref().borrow().get(token);
+            return parent.as_ref().get(token);
         }
-        Err(EnvError::NotFound {
-            token: token.clone(),
-        })
+        Err(EnvError::not_found(token))
     }
 }
 
@@ -103,6 +116,24 @@ mod tests {
 
         env.assign("fzz", "dzz").unwrap();
         assert_eq!(env.get(&id("fzz")).unwrap(), "dzz".into());
+    }
+
+    #[test]
+    fn test_nested_assign() {
+        let env = Env::default();
+        env.define("foo", "bar");
+        env.assign("foo", "bar2");
+
+        let child = env.child();
+        child.define("foo", "bar");
+        assert_eq!(child.get(&id("foo")).unwrap(), "bar".into());
+
+        let child = env.child();
+        assert_eq!(child.get(&id("foo")).unwrap(), "bar2".into());
+        child.assign("foo", "bar3");
+        assert_eq!(child.get(&id("foo")).unwrap(), "bar3".into());
+
+        assert_eq!(env.get(&id("foo")).unwrap(), "bar3".into());
     }
 
     fn id(name: &str) -> Token {
